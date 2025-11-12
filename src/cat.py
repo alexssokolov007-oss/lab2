3593956cf32e8da91300f5b38ba8390d1129c9b1
alexssokolov007-oss <alexssokolov007@gmail.com>
2025-11-12 13:48:47 +0300

    Update cat.py

--- src/cat.py
+++ src/cat.py
@@ -3,7 +3,8 @@ from errors import validate_path_exists, validate_is_file
 from constants import MAX_FILE_SIZE
 
 
-def cat(file_path: str) -> str:  '''Выводит содержимое файла'''
+def cat(file_path: str) -> str: 
+    '''Выводит содержимое файла'''
     path_obj = Path(file_path)
     validate_path_exists(path_obj)
     validate_is_file(path_obj)
